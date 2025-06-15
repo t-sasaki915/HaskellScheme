c9034eb7eb248d8c84c350c55013ff2597714067
Toma Sasaki <netst915@gmail.com>
2025-06-15 13:44:15 +0900

    update main

--- app/Main.hs
+++ app/Main.hs
@@ -3,6 +3,37 @@ module Main (main) where
 import qualified Data.Text              as Text
 import qualified Data.Text.IO           as TextIO
 import           Language.Scheme.Parser (parseScheme)
+import           Options.Applicative
+
+newtype AppOption = AppOption
+    { sourceFilePath :: Maybe FilePath
+    }
+
+argumentParser :: Parser AppOption
+argumentParser = AppOption
+    <$> optional
+        ( strOption
+            ( long "src"
+           <> short 's'
+           <> help "Filepath to Scheme source to interpret."
+           <> metavar "FilePath"
+            )
+        )
+
+appMain :: AppOption -> IO ()
+appMain opts =
+    case sourceFilePath opts of
+        Just srcPath ->
+            TextIO.readFile srcPath >>= \schemeSource ->
+                TextIO.putStrLn $ Text.show $ parseScheme schemeSource
+
+        Nothing ->
+            error "Not implemented"
 
 main :: IO ()
-main = TextIO.putStrLn $ Text.show $ parseScheme "(asdf) aaa"
+main = appMain =<< execParser opts
+    where
+        opts = info (argumentParser <**> helper)
+            ( fullDesc
+           <> header "HaskellScheme Interpreter"
+            )
